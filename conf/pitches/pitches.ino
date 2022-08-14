eef5b07271daa40e86ce9126d3979d15b261dfc2
cheungtobias0209 <88350604+cheungtobias0209@users.noreply.github.com>
2022-08-14 14:45:44 +0200

    update
    1. added scripts folder and conf folder

---
+++ conf/pitches/pitches.ino
@@ -0,0 +1,86 @@
+/*
+
+  Melody
+
+  Plays a melody
+
+  circuit:
+
+  - 8 ohm speaker on digital pin 8
+
+  created 21 Jan 2010
+
+  modified 30 Aug 2011
+
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cchttps://www.arduino.cc/en/Tutorial/Tone
+
+*/
+
+#include "pitches.h"
+
+// notes in the melody:
+
+
+//##############**"HE IS A PIRATE" Theme song of Pirates of caribbean**##############//
+int melody[] = {
+NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4,
+NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, 
+NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4,  
+NOTE_A3, NOTE_C4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_F4, 
+NOTE_F4, NOTE_G4, NOTE_E4, NOTE_E4, NOTE_D4, NOTE_C4, NOTE_C4, NOTE_D4,
+0, NOTE_A3, NOTE_C4, NOTE_B3, NOTE_D4, NOTE_B3, NOTE_E4, NOTE_F4,
+NOTE_F4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4,
+NOTE_D4, 0, 0, NOTE_A3, NOTE_C4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_F4,
+NOTE_G4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_G4,
+NOTE_A4, NOTE_D4, 0, NOTE_D4, NOTE_E3, NOTE_F4, NOTE_F4, NOTE_G4, NOTE_A4, 
+NOTE_D4, 0, NOTE_D4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_F4, NOTE_D4
+};
+int noteDurations[] = {
+4,8,4,8,4,8,8,8,8,4,8,4,8,4,8,8,8,8,4,8,4,8,
+4,8,8,8,8,4,4,8,8,4,4,8,8,4,4,8,8,
+8,4,8,8,8,4,4,8,8,4,4,8,8,4,4,8,4,
+4,8,8,8,8,4,4,8,8,4,4,8,8,4,4,8,8,
+8,4,8,8,8,4,4,4,8,4,8,8,8,4,4,8,8
+};
+//###########End of He is a Pirate song#############//
+
+
+
+void setup() {
+
+  // iterate over the notes of the melody:
+
+  
+}
+
+void loop() {
+
+for (int thisNote = 0; thisNote < sizeof(melody); thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+
+    int noteDuration = 1000 / noteDurations[thisNote];
+
+    tone(18, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+
+    // the note's duration + 30% seems to work well:
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    // stop the tone playing:
+
+    noTone(18);
+
+  }
+  // no need to repeat the melody.
+}
